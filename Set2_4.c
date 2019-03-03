b08c98f9e628435fde6013dbaaab9513c7a72ed0
deepikasixface <47886937+deepikasixface@users.noreply.github.com>
2019-03-03 13:33:06 +0530

    Create Set2_4.c

---
+++ Set2_4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main ()
+{
+char a [100];
+int i,l;
+scanf ("%s",a);
+l=strlen (a);
+for (i=l-1; i>=0;i--)
+{
+if (a [i]!='a'&a[i]!=e&d a[i]!='i'e& a[i]!='0'&& a[i]!=i)
+{
+printf ("%e", a[i]);
+}
+getch();
+}
